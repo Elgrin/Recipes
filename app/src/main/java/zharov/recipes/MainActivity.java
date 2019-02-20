d3faf31b3b0cb9f71d1fdde7de4e92ef243b3827
Михаил <mzhar25@gmail.com>
2019-02-20 15:46:49 +0300

    Bugs fixed. Menu item for favs created

--- app/src/main/java/zharov/recipes/MainActivity.java
+++ app/src/main/java/zharov/recipes/MainActivity.java
@@ -75,6 +75,29 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                
+                Intent intent = new Intent(MainActivity.this, FavsList.class);
+                startActivity(intent);
+
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public boolean isOnline() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
